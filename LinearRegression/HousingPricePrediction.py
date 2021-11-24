681427af9fd091c8f7c735b93cb72b09056fc83c
Robert Roman <robertroman7@gmail.com>
2021-11-24 02:01:24 -0600

    housing prediction first commit

---
+++ LinearRegression/HousingPricePrediction.py
@@ -0,0 +1,26 @@
+import os
+import tarfile
+import urllib.request
+import pandas as pd
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+fetch_housing_data()
+print(load_housing_data())
